09a3edd84667cb3b3932fecb033b5b0428d60679
DOLPHIN <ashraful.maruf.anik@gmail.com>
2019-09-27 01:40:16 +0600

    rent details

---
+++ app/src/main/java/com/anik/armr/rentmate/Activity/RentDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.anik.armr.rentmate.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.anik.armr.rentmate.R;
+
+public class RentDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rent_details);
+    }
+}
